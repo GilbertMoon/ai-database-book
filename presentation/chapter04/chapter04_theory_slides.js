8c3b5596f8a9b90e2d60ff50f4cb5ed61abaa949
Joyccino <67300266+joyccino@users.noreply.github.com>
2026-07-26 06:42:39 +0900

    chapter06

--- presentation/chapter04/chapter04_theory_slides.js
+++ presentation/chapter04/chapter04_theory_slides.js
@@ -1,2 +1,3 @@
 document.write('<script src="chapter04_theory_slides_raw.js"><\/script>');
+document.write('<script src="chapter04_intro_patch.js"><\/script>');
 document.write('<script src="../common/screen_position_patch.js"><\/script>');
--- presentation/chapter05/chapter05_theory_slides.js
+++ presentation/chapter05/chapter05_theory_slides.js
@@ -1,2 +1,3 @@
 document.write('<script src="chapter05_theory_slides_raw.js"><\/script>');
+document.write('<script src="chapter05_intro_patch.js"><\/script>');
 document.write('<script src="../common/screen_position_patch.js"><\/script>');
