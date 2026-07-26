4582b153d243df476fe0fdc5a5c8c20799368779
GilbertMoon <55611811+GilbertMoon@users.noreply.github.com>
2026-07-26 21:44:57 +0900

    Load expanded Chapter 03 theory popup scripts

--- presentation/chapter03/chapter03_theory_slides.js
+++ presentation/chapter03/chapter03_theory_slides.js
@@ -1,3 +1,4 @@
 document.write('<script src="chapter03_theory_slides_raw.js"><\/script>');
 document.write('<script src="chapter03_intro_patch.js"><\/script>');
+document.write('<script src="chapter03_theory_script_expansion.js"><\/script>');
 document.write('<script src="../common/screen_position_patch.js"><\/script>');
