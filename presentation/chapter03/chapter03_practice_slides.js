970d9fca36022c1d1c7bc8754d25ebcf102b8e4c
GilbertMoon <55611811+GilbertMoon@users.noreply.github.com>
2026-07-26 21:50:26 +0900

    Load expanded Chapter 03 practice popup scripts

--- presentation/chapter03/chapter03_practice_slides.js
+++ presentation/chapter03/chapter03_practice_slides.js
@@ -1,2 +1,3 @@
 document.write('<script src="chapter03_practice_slides_raw.js"><\/script>');
+document.write('<script src="chapter03_practice_script_expansion.js"><\/script>');
 document.write('<script src="../common/screen_position_patch.js"><\/script>');
